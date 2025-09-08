cf97057c14f160d9945364925abc0012e782f03d
Muju <anwn0414@naver.com>
2025-09-08 23:51:21 +0900

    치킨배달 문제풀이

---
+++ certi-practice/beakjoon_15686.cpp
@@ -0,0 +1,59 @@
+// 치킨 배달
+#include <iostream>
+#include <vector>
+using namespace std;
+int N, M, Map[51][51];
+vector<pair<int, int>> home, chicken;
+vector<vector<int>> chickenList;
+void init()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> N;
+	cin >> M;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> Map[i][j];
+			if (Map[i][j] == 1) home.push_back({ i, j });
+			if (Map[i][j] == 2) chicken.push_back({ i, j });
+		}
+	}
+}
+
+void combi(int start, vector<int> v) // 치킨 가게 m 수만큼 나올 조합
+{
+	if (v.size() == M)
+	{
+		chickenList.push_back(v);
+	}
+	for (int i = start + 1; i < chicken.size(); i++)
+	{
+		v.push_back(i);
+		combi(i, v);
+		v.pop_back();
+	}
+}
+
+int main()
+{
+	init();
+	vector<int> v;
+	int result = 1e9;
+	combi(-1, v);
+	for (vector<int> cList : chickenList) {
+		int ret = 0;
+		for (pair<int, int> home : home) {
+			int _min = 1e9;
+			for (int ch : cList) {
+				int _dist = abs(home.first - chicken[ch].first) + abs(home.second - chicken[ch].second);
+				_min = min(_min, _dist);
+			}
+			ret += _min;
+		}
+		result = min(result, ret);
+	}
+	cout << result << "\n";
+	return 0;
+}
--- certi-practice/beakjoon_17070.cpp
+++ certi-practice/beakjoon_17070.cpp
@@ -1,73 +1,73 @@
-// 파이프 옮기기 1 
-#include <cstring>
-#include <iostream>
-
-using namespace std;
-
-int map[16 + 1][16 + 1];
-int HouseSize;
-int dp[16 + 1][16 + 1][3]; // x, y, 방향(0 = 가로, 1 = 대각선, 2 = 세로)
-
-void init()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	memset(map, 0, sizeof(map));
-	memset(dp, 0, sizeof(dp));
-
-	cin >> HouseSize;
-	for (int i = 0; i < HouseSize; i++)
-	{
-		for (int j = 0; j < HouseSize; j++)
-		{
-			cin >> map[i][j];
-		}
-	}
-}
-bool check(int x, int y, int dir)
-{
-	switch (dir)
-	{
-	case 0:
-	case 2:
-		if ((map[x][y] == 0))
-			return true;
-		break;
-	case 1:
-		if ((map[x - 1][y] == 0) && (map[x][y - 1] == 0) && (map[x][y] == 0))
-			return true;
-		break;
-	default:
-		return false;
-		break;
-	}
-	return false;
-}
-int calculate()
-{
-	dp[2][1][0] = 1;
-	for (int i = 1; i <= HouseSize; i++)
-	{
-		for (int j = 1; j <= HouseSize; j++)
-		{
-			// 가로일 경우
-			if (check(i + 1, j, 0)) dp[i + 1][j][0] += dp[i][j][0];
-			if (check(i + 1, j + 1, 1)) dp[i + 1][j + 1][1] += dp[i][j][0];
-			// 세로일 경우
-			if (check(i, j + 1, 2)) dp[i][j + 1][2] += dp[i][j][2];
-			if (check(i + 1, j + 1, 2)) dp[i + 1][j + 1][1] += dp[i][j][2];
-			// 대각선일 경우
-			if (check(i + 1, j, 1)) dp[i + 1][j][0] += dp[i][j][1];
-			if (check(i + 1, j, 1)) dp[i][j + 1][2] += dp[i][j][1];
-			if (check(i + 1, j, 1)) dp[i + 1][i + 1][1] += dp[i][j][1];
-		}
-	}
-	return dp[HouseSize][HouseSize][0] + dp[HouseSize][HouseSize][1] + dp[HouseSize][HouseSize][2];
-}
-
-int main()
-{
-	init();
-	cout << calculate();
-}
+//// 파이프 옮기기 1 //실패 - 다음에 다시 풀기..
+//#include <cstring>
+//#include <iostream>
+//
+//using namespace std;
+//
+//int map[16 + 1][16 + 1];
+//int HouseSize;
+//int dp[16 + 1][16 + 1][3]; // x, y, 방향(0 = 가로, 1 = 대각선, 2 = 세로)
+//
+//void init()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	memset(map, 0, sizeof(map));
+//	memset(dp, 0, sizeof(dp));
+//
+//	cin >> HouseSize;
+//	for (int i = 0; i < HouseSize; i++)
+//	{
+//		for (int j = 0; j < HouseSize; j++)
+//		{
+//			cin >> map[i][j];
+//		}
+//	}
+//}
+//bool check(int x, int y, int dir)
+//{
+//	switch (dir)
+//	{
+//	case 0:
+//	case 2:
+//		if ((map[x][y] == 0))
+//			return true;
+//		break;
+//	case 1:
+//		if ((map[x - 1][y] == 0) && (map[x][y - 1] == 0) && (map[x][y] == 0))
+//			return true;
+//		break;
+//	default:
+//		return false;
+//		break;
+//	}
+//	return false;
+//}
+//int calculate()
+//{
+//	dp[2][1][0] = 1;
+//	for (int i = 1; i <= HouseSize; i++)
+//	{
+//		for (int j = 1; j <= HouseSize; j++)
+//		{
+//			// 가로일 경우
+//			if (check(i + 1, j, 0)) dp[i + 1][j][0] += dp[i][j][0];
+//			if (check(i + 1, j + 1, 1)) dp[i + 1][j + 1][1] += dp[i][j][0];
+//			// 세로일 경우
+//			if (check(i, j + 1, 2)) dp[i][j + 1][2] += dp[i][j][2];
+//			if (check(i + 1, j + 1, 2)) dp[i + 1][j + 1][1] += dp[i][j][2];
+//			// 대각선일 경우
+//			if (check(i + 1, j, 1)) dp[i + 1][j][0] += dp[i][j][1];
+//			if (check(i + 1, j, 1)) dp[i][j + 1][2] += dp[i][j][1];
+//			if (check(i + 1, j, 1)) dp[i + 1][i + 1][1] += dp[i][j][1];
+//		}
+//	}
+//	return dp[HouseSize][HouseSize][0] + dp[HouseSize][HouseSize][1] + dp[HouseSize][HouseSize][2];
+//}
+//
+//int main()
+//{
+//	init();
+//	cout << calculate();
+//}
