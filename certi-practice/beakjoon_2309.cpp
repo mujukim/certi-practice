5c3b2598fced09e499326fb28a3b7082a0457fa4
Muju <anwn0414@naver.com>
2025-09-12 23:13:05 +0900

    2979 문제 풀이 성공

--- certi-practice/beakjoon_2309.cpp
+++ certi-practice/beakjoon_2309.cpp
@@ -1,50 +1,50 @@
-// 일곱난쟁이
-
-// 조합으로 풀기
-// 9개 중 7개 뽑아서 다 더해서 100이 될 때 그 수를 출력하기 
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
-int height[9];
-int a, b;
-void init()
-{
-	//시간절약
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	for (int i = 0; i < 9; i++)
-	{
-		cin >> height[i];
-	}
-}
-void combi()
-{
-	for (a = 0; a < 9; a++)
-	{
-		for (b = a + 1; b < 9; b++)
-		{
-			if (a == b) continue;
-
-			int sum = 0;
-			for (int k = 0; k < 9; k++)
-			{
-				if ((k == a) || (k == b)) continue;
-				sum += height[k];
-			}
-			if (sum == 100) return;
-		}
-	}
-}
-int main()
-{
-	init();
-	sort(height, height + 9);
-	combi();
-	for (int i = 0; i < 9; i++)
-	{
-		if ((i == a) || (i == b)) continue;
-		cout << height[i] << "\n";
-	}
-	return 0;
-}
+//// 일곱난쟁이
+//
+//// 조합으로 풀기
+//// 9개 중 7개 뽑아서 다 더해서 100이 될 때 그 수를 출력하기 
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//int height[9];
+//int a, b;
+//void init()
+//{
+//	//시간절약
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	for (int i = 0; i < 9; i++)
+//	{
+//		cin >> height[i];
+//	}
+//}
+//void combi()
+//{
+//	for (a = 0; a < 9; a++)
+//	{
+//		for (b = a + 1; b < 9; b++)
+//		{
+//			if (a == b) continue;
+//
+//			int sum = 0;
+//			for (int k = 0; k < 9; k++)
+//			{
+//				if ((k == a) || (k == b)) continue;
+//				sum += height[k];
+//			}
+//			if (sum == 100) return;
+//		}
+//	}
+//}
+//int main()
+//{
+//	init();
+//	sort(height, height + 9);
+//	combi();
+//	for (int i = 0; i < 9; i++)
+//	{
+//		if ((i == a) || (i == b)) continue;
+//		cout << height[i] << "\n";
+//	}
+//	return 0;
+//}
---
+++ certi-practice/beakjoon_2979.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int a, b, c;
+int timearr[101];
+int max_time;
+void init()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	memset(timearr, 0x00, sizeof(timearr));
+	int start_, end_;
+	max_time = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> start_ >> end_;
+		if (max_time < end_) max_time = end_;
+		for (int j = start_; j < end_; j++)
+		{
+			timearr[j]++;
+		}
+	}
+}
+
+int solve()
+{
+	int ret = 0;
+	for (int i = 1; i <= max_time; i++)
+	{	
+		if (timearr[i] == 1) ret += a;
+		else if (timearr[i] == 2) ret += (b * 2);
+		else if (timearr[i] == 3) ret += (c * 3);
+	}
+	return ret;
+}
+
+int main()
+{
+	init();
+	cout << solve();
+}
