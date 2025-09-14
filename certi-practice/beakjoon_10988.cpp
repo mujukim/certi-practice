397c9d6526e25fb2981315bb46935c4fb0a5b1ad
Muju <anwn0414@naver.com>
2025-09-14 23:10:04 +0900

    1159 문제 풀이

--- certi-practice/beakjoon_10988.cpp
+++ certi-practice/beakjoon_10988.cpp
@@ -1,20 +1,20 @@
-#include<iostream>
-
-using namespace std;
-
-int main()
-{
-	string input;
-	cin >> input;
-	int strsize = input.size();
-	int ret = 1;
-	for (int i = 0; i < strsize / 2; i++)
-	{
-		if (input[i] != input[strsize - 1 - i])
-		{
-			ret = 0;
-			break;
-		}
-	}
-	cout << ret;
-}
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string input;
+//	cin >> input;
+//	int strsize = input.size();
+//	int ret = 1;
+//	for (int i = 0; i < strsize / 2; i++)
+//	{
+//		if (input[i] != input[strsize - 1 - i])
+//		{
+//			ret = 0;
+//			break;
+//		}
+//	}
+//	cout << ret;
+//}
