00dea6bb3c3edf86c1abf58b2866ffe9988298a5
Muju <anwn0414@naver.com>
2025-09-22 23:56:02 +0900

    1620 번 문제 풀이

---
+++ certi-practice/beakjoon_1620.cpp
@@ -0,0 +1,28 @@
+//c_str() : string->char·Î º¯È¯
+
+#include <iostream>
+#include <map>
+using namespace std;
+int n, m;
+string s;
+map<string, int> mp;
+string a[100004];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		mp[s] = i + 1;
+		a[i + 1] = s;
+	}
+	for (int i = 0; i < m; i++) {
+		cin >> s;
+		if (atoi(s.c_str()) == 0) {
+			cout << mp[s] << "\n";
+		}
+		else {
+			cout << a[atoi(s.c_str())] << "\n";
+		}
+	}
+}
--- certi-practice/beakjoon_2559.cpp
+++ certi-practice/beakjoon_2559.cpp
@@ -1,31 +1,31 @@
-#include <iostream>
-
-using namespace std;
-
-int N, K, psum[100001];
-
-int calc()
-{
-	int ret = -1e9;
-	for (int i = K; i <= N; i++)
-	{
-		ret = max(ret, psum[i] - psum[i - K]);
-	}
-	return ret;
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cin >> N >> K;
-	int input;
-	psum[0] = 0;
-	for (int i = 1; i <= N; i++)
-	{
-		cin >> input;
-		psum[i] = psum[i - 1] + input;
-	}
-	cout << calc();
-	return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int N, K, psum[100001];
+//
+//int calc()
+//{
+//	int ret = -1e9;
+//	for (int i = K; i <= N; i++)
+//	{
+//		ret = max(ret, psum[i] - psum[i - K]);
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cin >> N >> K;
+//	int input;
+//	psum[0] = 0;
+//	for (int i = 1; i <= N; i++)
+//	{
+//		cin >> input;
+//		psum[i] = psum[i - 1] + input;
+//	}
+//	cout << calc();
+//	return 0;
+//}
