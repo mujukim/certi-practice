064ce03202221ed957da56e15301cfb989e082b6
Muju <anwn0414@naver.com>
2025-09-29 23:51:07 +0900

    1213 문제 풀이

--- certi-practice/beakjoon_9357.cpp
+++ certi-practice/beakjoon_9357.cpp
@@ -1,28 +1,28 @@
-#include <iostream>
-#include <map>
-using namespace std;
-int n, tc;
-map<string, int> clothes;
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cin >> tc;
-	string temp, temp2;
-	for (int i = 0; i < tc; i++)
-	{
-		clothes.clear();
-		cin >> n;
-		for (int j = 0; j < n; j++)
-		{
-			cin >> temp >> temp2;
-			clothes[temp2]++;
-		}
-		long long ret = 1; // 경우의수면 무조건 수가 커질수 있기 때문에 long long을 한다.
-		for (auto c : clothes) {
-			ret *= ((long long)c.second + 1); // 안입는 경우 포함하기 위해 + 1
-		}
-		ret--; // 아무것도 안입는 경우 삭제
-		cout << ret << "\n";
-	}
-}
+//#include <iostream>
+//#include <map>
+//using namespace std;
+//int n, tc;
+//map<string, int> clothes;
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cin >> tc;
+//	string temp, temp2;
+//	for (int i = 0; i < tc; i++)
+//	{
+//		clothes.clear();
+//		cin >> n;
+//		for (int j = 0; j < n; j++)
+//		{
+//			cin >> temp >> temp2;
+//			clothes[temp2]++;
+//		}
+//		long long ret = 1; // 경우의수면 무조건 수가 커질수 있기 때문에 long long을 한다.
+//		for (auto c : clothes) {
+//			ret *= ((long long)c.second + 1); // 안입는 경우 포함하기 위해 + 1
+//		}
+//		ret--; // 아무것도 안입는 경우 삭제
+//		cout << ret << "\n";
+//	}
+//}
