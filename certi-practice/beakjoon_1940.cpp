3570aa8adb27ecbf697dbc8508c902734243359d
Muju <anwn0414@naver.com>
2025-10-01 22:59:04 +0900

    3986 문제풀이

--- certi-practice/beakjoon_1940.cpp
+++ certi-practice/beakjoon_1940.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-int n, m;
-int arr[15001];
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cin >> n >> m;
-	int temp = 0;
-	int result = 0;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> arr[i];
-	}
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (arr[i] + arr[j] == m)
-				result++;
-		}
-	}
-	cout << result << "\n";
-
-}
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//int n, m;
+//int arr[15001];
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cin >> n >> m;
+//	int temp = 0;
+//	int result = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//	for (int i = 0; i < n; i++)
+//	{
+//		for (int j = i + 1; j < n; j++)
+//		{
+//			if (arr[i] + arr[j] == m)
+//				result++;
+//		}
+//	}
+//	cout << result << "\n";
+//
+//}
