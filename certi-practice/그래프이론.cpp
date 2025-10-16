48eceddd73007903957d616e0b64efe46cabc03b
Muju <anwn0414@naver.com>
2025-10-16 23:28:03 +0900

    좌표탐색

--- certi-practice/그래프이론.cpp
+++ certi-practice/그래프이론.cpp
@@ -66,3 +66,42 @@ void init()
 
     adj[3].push_back(0);
 }
+
+//-> 보통은 인접리스트로 풀고 문제에 행렬로 나왔을때만 행렬로 풀자!
+
+//좌표 탐색
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+int main()
+{
+    int y = 0, x = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = y + dy[i];
+        int ny = x + dy[i];
+    }
+}
+
+//Q. 3 * 3 맵을 입력받아야 함. 이 맵은 1과 0으로 이루어져있고 {0, 0}은 무조건 1임을 보장한다.
+//{0, 0}부터 4방향을 기준으로 한칸씩 탐색해나가며 방문한 정점은 다시 방문하지 않으며 방문하는 좌표를 출력하는 코드.
+//0은 갈 수 없는 지역. 1은 갈 수 있는 지역을 구현하시오.
+
+const int n = 3;
+int a[n][n];
+int visited_[n][n] = { 0, };
+const int dy[] = { -1, 0, 1, 0 };
+const int dx[] = { 0, -1, 0, 1 };
+ 
+void go(int y, int x) {
+    visited_[y][x] = 1;
+    cout << y << " : " << x << "\n"; // 방문한 좌표 출력
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+        if (a[ny][nx] == 0) continue;
+        if (visited_[ny][nx]) continue;
+        go(ny, nx);
+    }
+    return;
+}
