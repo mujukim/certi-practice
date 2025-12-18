f31ad9bd041c634c66c5d57448b53a8af47915b8
Muju <anwn0414@naver.com>
2025-12-18 23:52:53 +0900

    중간커밋

--- certi-practice/beakjoon_2583.cpp
+++ certi-practice/beakjoon_2583.cpp
@@ -1,15 +1,27 @@
 // 영역구하기
 
 #include<iostream>
-#define MAX_ 100
+#define MAX_ 102
 using namespace std;
 
 int M = 0, N = 0, K = 0;
-int map[MAX_][MAX_];
-int sx[MAX_];
-int sy[MAX_];
-int ex[MAX_];
-int ey[MAX_];
+int map[MAX_][MAX_], visited[MAX_][MAX_];
+int sx;
+int sy;
+int ex;
+int ey;
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, -1, 0, 1 };
+int dfs(int y, int x)
+{
+    visited[y][x] = 1;
+    int ret = 1;
+    for (int i = 0; i < 4; i++)
+    {
+
+    }
+
+}
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -18,26 +30,20 @@ int main() {
     cin >> M >> N >> K;
     for (int i = 0; i < K; i++)
     {
-        cin >> sx[i] >> sy[i] >> ex[i] >> ey[i];
-    }
-
-    // map에 rect 채우기
-    for (int m = 0; m < M; m++)
-    {
-        for (int n = 0; n < N; n++)
+        cin >> sx >> sy >> ex >> ey;
+        for (int x = sx; x < ex; x++)
         {
-            for (int k = 0; k < K; k++)
-            {           
-                if ((map[m][n] >= sx[k] && map[m][n] < ex[k]) &&
-                    (map[m][n] >= sy[k] && map[m][n] < ey[k]))
-                {
-                    map[m][n] = 1;
-                }
-                else
-                {
-                    map[m][n] = 0;
-                }
+            for (int y = sy; y < ey; y++)
+            {
+                map[y][x] = 1;
             }
+        }
+    }
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+
         }
     }
   
