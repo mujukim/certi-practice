bb1a435db7b978636115c5c22af3d55dd561995e
Muju <anwn0414@naver.com>
2025-12-19 23:25:08 +0900

    2583 완료

--- certi-practice/241213_Certi.cpp
+++ certi-practice/241213_Certi.cpp
@@ -1,70 +1,70 @@
-// 상품을 랭킹순으로 정렬
-// id를 기준으로 상품을 제거
-// id를 기준으로 상품의 랭킹을 변경
-// 카테코리 내 두 랭크 상품 전체 교체
-
-// ## 자료구조
-// unordered_map<int, pair<int, int>> GoodsInfo;  
-// key : ID, value : (category, rank)
-
-// set<int> RankSet[category 개수][랭킹 개수] = 들어있는건 ID 집합(오름차순 정렬됨)
-// RankSet[1][2].insert(10);
-// RankSet[1][2].insert(20);
-// RankSet[1][1].insert(30);
-//   -> category 1, rank 2 = {20, 10}
-//   -> category 1, rank 1 = {30}
-
-
-#include <iostream>
-#include <unordered_map>
-#include <set>
-using namespace std;
-
-struct ResultST
-{
-	int mID[3];
-};
-unordered_map<int, pair<int, int>> goodsInfo;
-set<int> catRankSet[6][6];
-set<pair<int, int>> total;
-
-void init()
-{
-	goodsInfo.clear();
-	total.clear();
-	for (int i = 0; i < 6; i++)
-	{
-		for (int j = 0; j < 6; j++)
-		{
-			catRankSet[i][j].clear();
-		}
-	}
-}
-
-int add(int mGoodsID, int mCategory, int mRank)
-{
-	goodsInfo[mGoodsID] = make_pair(mCategory, mRank);
-	catRankSet[mCategory][mRank].insert(mGoodsID);
-	total.insert(make_pair(mRank, -mGoodsID)); // rank 순으로 배열하기 위해서 -mGoodsID를 함
-
-}
-
-int remove(int mGoodsID)
-{
-
-}
-
-int changeRank(int mGoodsID, int mRank)
-{
-
-}
-
-int swapRank(int category, int mRank1, int mRank2)
-{
-
-}
-
-ResultST GetTopRanks()
-{
-
-}
+//// 상품을 랭킹순으로 정렬
+//// id를 기준으로 상품을 제거
+//// id를 기준으로 상품의 랭킹을 변경
+//// 카테코리 내 두 랭크 상품 전체 교체
+//
+//// ## 자료구조
+//// unordered_map<int, pair<int, int>> GoodsInfo;  
+//// key : ID, value : (category, rank)
+//
+//// set<int> RankSet[category 개수][랭킹 개수] = 들어있는건 ID 집합(오름차순 정렬됨)
+//// RankSet[1][2].insert(10);
+//// RankSet[1][2].insert(20);
+//// RankSet[1][1].insert(30);
+////   -> category 1, rank 2 = {20, 10}
+////   -> category 1, rank 1 = {30}
+//
+//
+//#include <iostream>
+//#include <unordered_map>
+//#include <set>
+//using namespace std;
+//
+//struct ResultST
+//{
+//	int mID[3];
+//};
+//unordered_map<int, pair<int, int>> goodsInfo;
+//set<int> catRankSet[6][6];
+//set<pair<int, int>> total;
+//
+//void init()
+//{
+//	goodsInfo.clear();
+//	total.clear();
+//	for (int i = 0; i < 6; i++)
+//	{
+//		for (int j = 0; j < 6; j++)
+//		{
+//			catRankSet[i][j].clear();
+//		}
+//	}
+//}
+//
+//int add(int mGoodsID, int mCategory, int mRank)
+//{
+//	goodsInfo[mGoodsID] = make_pair(mCategory, mRank);
+//	catRankSet[mCategory][mRank].insert(mGoodsID);
+//	total.insert(make_pair(mRank, -mGoodsID)); // rank 순으로 배열하기 위해서 -mGoodsID를 함
+//
+//}
+//
+//int remove(int mGoodsID)
+//{
+//
+//}
+//
+//int changeRank(int mGoodsID, int mRank)
+//{
+//
+//}
+//
+//int swapRank(int category, int mRank1, int mRank2)
+//{
+//
+//}
+//
+//ResultST GetTopRanks()
+//{
+//
+//}
--- certi-practice/beakjoon_2583.cpp
+++ certi-practice/beakjoon_2583.cpp
@@ -1,9 +1,12 @@
 // 영역구하기
 
 #include<iostream>
+#include<vector>
+#include<algorithm>
 #define MAX_ 102
 using namespace std;
 
+vector<int> ret;
 int M = 0, N = 0, K = 0;
 int map[MAX_][MAX_], visited[MAX_][MAX_];
 int sx;
@@ -12,15 +15,20 @@ int ex;
 int ey;
 const int dx[4] = { -1, 0, 1, 0 };
 const int dy[4] = { 0, -1, 0, 1 };
+
 int dfs(int y, int x)
 {
     visited[y][x] = 1;
     int ret = 1;
     for (int i = 0; i < 4; i++)
     {
-
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny < 0 || ny >= M || nx < 0 || nx >= N || visited[ny][nx] == 1) continue;
+        if (map[ny][nx] == 1) continue;
+        ret += dfs(ny, nx);
     }
-
+    return ret;
 }
 int main() {
     ios_base::sync_with_stdio(false);
@@ -43,9 +51,12 @@ int main() {
     {
         for (int j = 0; j < N; j++)
         {
-
+            if ((map[i][j] != 1) && (visited[i][j] == 0)) {
+                ret.push_back(dfs(i, j));
+            }
         }
     }
-  
-    // 영역찾아내기
+    sort(ret.begin(), ret.end());
+    cout << ret.size() << "\n";
+    for (int a : ret) cout << a << " ";
 }
