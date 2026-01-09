763e70ba2751567630323ed83d72625d4fa7db50
Muju <anwn0414@naver.com>
2026-01-09 23:43:06 +0900

    2828 문제 풀이

--- certi-practice/beakjoon_1992.cpp
+++ certi-practice/beakjoon_1992.cpp
@@ -1,41 +1,41 @@
-//ÄõµåÆ®¸®
-
-#include <iostream>
-using namespace std;
-typedef long long int ll;
-int n;
-string s;
-char map[101][101];
-
-string quard(int y, int x, int size) {
-    if (size == 1) return string(1, map[y][x]);
-    char b = map[y][x];
-    string ret = "";
-    for (int i = y; i < y + size; i++) {
-        for (int j = x; j < x + size; j++) {
-            if (b != map[i][j]) {
-                ret += '(';
-                ret += quard(y, x, size / 2);
-                ret += quard(y, x + size / 2, size / 2);
-                ret += quard(y + size / 2, x, size / 2);
-                ret += quard(y + size / 2, x + size / 2, size / 2);
-                ret += ')';
-                return ret;
-            }
-        }
-    }
-    return string(1, map[y][x]);
-}
-
-int main() {
-    ios_base::sync_with_stdio(0); cin.tie(NULL);
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> s;
-        for (int j = 0; j < n; j++) {
-            map[i][j] = s[j];
-        }
-    }
-    cout << quard(0, 0, n) << '\n';
-    return 0;
-}
+////ÄõµåÆ®¸®
+//
+//#include <iostream>
+//using namespace std;
+//typedef long long int ll;
+//int n;
+//string s;
+//char map[101][101];
+//
+//string quard(int y, int x, int size) {
+//    if (size == 1) return string(1, map[y][x]);
+//    char b = map[y][x];
+//    string ret = "";
+//    for (int i = y; i < y + size; i++) {
+//        for (int j = x; j < x + size; j++) {
+//            if (b != map[i][j]) {
+//                ret += '(';
+//                ret += quard(y, x, size / 2);
+//                ret += quard(y, x + size / 2, size / 2);
+//                ret += quard(y + size / 2, x, size / 2);
+//                ret += quard(y + size / 2, x + size / 2, size / 2);
+//                ret += ')';
+//                return ret;
+//            }
+//        }
+//    }
+//    return string(1, map[y][x]);
+//}
+//
+//int main() {
+//    ios_base::sync_with_stdio(0); cin.tie(NULL);
+//    cin >> n;
+//    for (int i = 0; i < n; i++) {
+//        cin >> s;
+//        for (int j = 0; j < n; j++) {
+//            map[i][j] = s[j];
+//        }
+//    }
+//    cout << quard(0, 0, n) << '\n';
+//    return 0;
+//}
